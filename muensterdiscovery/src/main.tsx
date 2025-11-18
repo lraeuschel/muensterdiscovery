9f0b8d6473b544560e3d37213ccdb34042e7973e
lraeusch <l.raeuschel@uni-muenster.de>
2025-11-18 20:17:42 +0100

    set up more pages

--- muensterdiscovery/src/main.tsx
+++ muensterdiscovery/src/main.tsx
@@ -1,7 +1,7 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 import Welcome from "./pages/welcome";
 import Login from "./pages/login";
 import Registration from "./pages/registration";
@@ -9,20 +9,25 @@ import OpenWorld from "./pages/openworld";
 import RouteSelection from "./pages/routeselection";
 import RouteDisplay from "./pages/routedisplay";
 import Leaderboard from "./pages/leaderboard";
+import Impressum from "./pages/impressum";
+import Help from "./pages/help";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/openworld" element={<OpenWorld />} />
-        <Route path="/routeselection" element={<RouteSelection />} />
-        <Route path="/routedisplay" element={<RouteDisplay />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+            <BrowserRouter basename="/muensterdiscovery">
+                <Routes>
+                    <Route path="/" element={<Welcome />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/registration" element={<Registration />} />
+                    <Route path="/openworld" element={<OpenWorld />} />
+                    <Route path="/routeselection" element={<RouteSelection />} />
+                    <Route path="/routedisplay" element={<RouteDisplay />} />
+                    <Route path="/leaderboard" element={<Leaderboard />} />
+                    <Route path="/impressum" element={<Impressum />} />
+                    <Route path="/help" element={<Help />} />
+                </Routes>
+            </BrowserRouter>
+        </ChakraProvider>
+    </React.StrictMode>
 );
--- muensterdiscovery/src/pages/welcome.tsx
+++ muensterdiscovery/src/pages/welcome.tsx
@@ -1,7 +1,64 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Box, Button, VStack, HStack, Select, Text, Image } from "@chakra-ui/react";
+import muensterdiscovery_logo from "../assets/muensterdiscovery_logo.jpg";
+
 export default function Welcome() {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h1>Welcome to Münster Discovery</h1>
-        </div>
-    )
+        <Box bg="orange.50" minH="100vh">
+            <VStack>
+                    <Text
+                        fontSize={{ base: "2xl", md: "4xl" }}
+                        fontWeight="bold"
+                        color="orange.500"
+                        letterSpacing="wide"
+                        textAlign="center"
+                        textTransform="uppercase"
+                        mb={4}
+                        mt={8}
+                        >
+                        Welcome to
+                    </Text>
+                <Image src={muensterdiscovery_logo} alt="Muenster Discovery Logo" boxSize="400px" mt={3} />
+                <Button
+                    colorPalette={"orange"}
+                    size="lg"
+                    onClick={() => navigate("/login")}
+                    width={"160px"}
+                >
+                    Login
+                </Button>
+                    <Text color="gray.500" fontWeight="semibold" fontSize="sm" whiteSpace="nowrap">
+                        or
+                    </Text>
+                <Button
+                    colorPalette={"orange"}
+                    size="lg"
+                    onClick={() => navigate("/registration")}
+                    width={"160px"}
+                >
+                    Create an account
+                </Button>
+                <Button
+                    colorPalette={"transparent"}
+                    size="lg"
+                    onClick={() => navigate("/help")}
+                    width={"160px"}
+                    mt={15}
+                >
+                    Need help?
+                </Button>
+                <VStack mt={15}>
+                    <Text>
+                        @2025 muensterdiscovery
+                    </Text>
+                    <Link to="/impressum">
+                        Impressum
+                    </Link>
+                </VStack>
+            </VStack>
+        </Box>
+    );
 }
