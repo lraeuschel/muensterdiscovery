a45ed73b4cbdc1df47d2e33ea1aaf6204ea863bf
lraeusch <l.raeuschel@uni-muenster.de>
2025-11-19 22:08:00 +0100

    design the menu

--- muensterdiscovery/src/components/menu.tsx
+++ muensterdiscovery/src/components/menu.tsx
@@ -1,20 +1,22 @@
     import { useNavigate } from "react-router-dom";
-    import { Menu, Button, Portal, Box } from "@chakra-ui/react";
+    import { Menu, Button, Portal, Box, HStack } from "@chakra-ui/react";
     import { RxHamburgerMenu } from "react-icons/rx";
     import { IoIosHelpCircleOutline } from "react-icons/io";
     import { CiRoute } from "react-icons/ci";
     import { MdOutlineLeaderboard } from "react-icons/md";
     import { IoEarthOutline } from "react-icons/io5";
     import { CgProfile } from "react-icons/cg";
+    import { useIntl } from "react-intl";
 
     export default function MenuComponent() {
         const navigate = useNavigate();
+        const intl = useIntl();
 
         const menuItems = [
             { value: "profile", icon: <CgProfile size={20} />, path: "/profile" },
             { value: "open-world", icon: <IoEarthOutline size={20} />, path: "/openworld" },
             { value: "leaderboard", icon: <MdOutlineLeaderboard size={20} />, path: "/leaderboard" },
-            { value: "routes", icon: <CiRoute size={20} />, path: "/routeselection" },
+            { value: "routeselection", icon: <CiRoute size={20} />, path: "/routeselection" },
             { value: "help", icon: <IoIosHelpCircleOutline size={20} />, path: "/help" },
         ];
 
@@ -33,10 +35,15 @@
                                 {menuItems.map((item) => (
                                     <Menu.Item
                                         value={item.value}
-                                        onClick={() => navigate(item.path)} // <-- Hier wird navigiert
-                                        width={"fit-content"}
+                                        onClick={() => navigate(item.path)}
+                                        width={"full"}
                                     >
-                                        {item.icon}
+                                        <Box >
+                                            <HStack>
+                                                {intl.formatMessage({ id: `menu.${item.value}` })}
+                                                {item.icon}
+                                            </HStack>
+                                        </Box>
                                     </Menu.Item>
                                 ))}
                             </Menu.Content>
