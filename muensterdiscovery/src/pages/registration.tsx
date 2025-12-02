6cce522a66403b979885cab7aae2c1dc6146ed5a
lraeusch <l.raeuschel@uni-muenster.de>
2025-12-02 15:41:35 +0100

    Added layout for registration page

--- muensterdiscovery/src/pages/registration.tsx
+++ muensterdiscovery/src/pages/registration.tsx
@@ -1,80 +1,173 @@
 import { useState } from "react";
 import { supabase } from "../SupabaseClient";
-import { Input, Button, Text, Flex, Box, Heading } from "@chakra-ui/react";
 import {
-    FormControl,
-    FormLabel,
-} from "@chakra-ui/form-control";
-import Header from "../components/Header";
+    Input,
+    Button,
+    Text,
+    Flex,
+    Box,
+    Heading,
+    VStack
+} from "@chakra-ui/react";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
 
+import Header from "../components/Header";
+import { useIntl } from "react-intl";
 
 export default function Signup() {
+    const intl = useIntl();
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
     const [message, setMessage] = useState("");
 
     async function handleSignup(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
+        setMessage("");
 
-        const { data, error } = await supabase.auth.signUp({
+        const { data: signupData, error: signupError } = await supabase.auth.signUp({
             email,
             password,
+            options: {
+                emailRedirectTo: `${window.location.origin}/auth/callback`  
+            }
         });
 
-        console.log(data);
+        if (signupError) {
+            setMessage(signupError.message);
+            return;
+        }
 
-        if (error) setMessage(error.message);
-        else setMessage("Bestätigungs-E-Mail wurde gesendet!");
+        const userId = signupData.user?.id;
+
+        if (userId) {
+            const { error: profileError } = await supabase
+                .from("profiles")
+                .insert({
+                    id: userId,
+                    first_name: firstName,
+                    last_name: lastName,
+                    username: username,
+                    email: email,
+                });
+
+            if (profileError) {
+                setMessage(profileError.message);
+                return;
+            }
+        }
+
+        setMessage(intl.formatMessage({ id: "registration.email_confirmation_message" }));
     }
 
     return (
         <>
             <Header />
-            <Flex justify="center" align="center" minH="100vh" bg="gray.50">
+
+            <Flex justify="center" align="center" minH="100vh" bg="orange.50" pt="80px">
                 <Box
                     bg="white"
                     p={8}
-                    rounded="md"
-                    shadow="md"
+                    rounded="xl"
+                    shadow="lg"
                     width="100%"
-                    maxW="400px"
+                    maxW="420px"
+                    border="2px solid"
+                    borderColor="orange.200"
                 >
-                    <Heading mb={6} textAlign="center" size="lg">
-                        Registrieren
+                    <Heading
+                        mb={6}
+                        textAlign="center"
+                        size="lg"
+                        color="orange.600"
+                    >
+                        {intl.formatMessage({ id: "registration.title" })}
                     </Heading>
 
                     <form onSubmit={handleSignup}>
-                        <FormControl mb={4}>
-                            <FormLabel>Email</FormLabel>
-                            <Input
-                                type="email"
-                                placeholder="Email eingeben"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                required
-                            />
-                        </FormControl>
+                        <VStack>
 
-                        <FormControl mb={6}>
-                            <FormLabel>Passwort</FormLabel>
-                            <Input
-                                type="password"
-                                placeholder="Passwort eingeben"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required
-                            />
-                        </FormControl>
+                            <FormControl>
+                                <FormLabel color="orange.700">{intl.formatMessage({ id: "registration.firstname" })}</FormLabel>
+                                <Input
+                                    bg="orange.50"
+                                    borderColor="orange.300"
+                                    _focus={{ borderColor: "orange.500" }}
+                                    value={firstName}
+                                    onChange={(e) => setFirstName(e.target.value)}
+                                    required
+                                />
+                            </FormControl>
 
-                        <Button colorScheme="blue" width="100%" type="submit">
-                            Signup
-                        </Button>
+                            <FormControl>
+                                <FormLabel color="orange.700">{intl.formatMessage({ id: "registration.lastname" })}</FormLabel>
+                                <Input
+                                    bg="orange.50"
+                                    borderColor="orange.300"
+                                    _focus={{ borderColor: "orange.500" }}
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)}
+                                    required
+                                />
+                            </FormControl>
 
-                        {message && (
-                            <Text mt={4} textAlign="center" color="blue.600">
-                                {message}
-                            </Text>
-                        )}
+                            <FormControl>
+                                <FormLabel color="orange.700">{intl.formatMessage({ id: "registration.username" })}</FormLabel>
+                                <Input
+                                    bg="orange.50"
+                                    borderColor="orange.300"
+                                    _focus={{ borderColor: "orange.500" }}
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
+                                />
+                            </FormControl>
+
+                            <FormControl>
+                                <FormLabel color="orange.700">{intl.formatMessage({ id: "registration.email" })}</FormLabel>
+                                <Input
+                                    type="email"
+                                    bg="orange.50"
+                                    borderColor="orange.300"
+                                    _focus={{ borderColor: "orange.500" }}
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </FormControl>
+
+                            <FormControl>
+                                <FormLabel color="orange.700">{intl.formatMessage({ id: "registration.password" })}</FormLabel>
+                                <Input
+                                    type="password"
+                                    bg="orange.50"
+                                    borderColor="orange.300"
+                                    _focus={{ borderColor: "orange.500" }}
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </FormControl>
+
+                            <Button
+                                colorScheme="orange"
+                                width="100%"
+                                rounded="lg"
+                                size="lg"
+                                type="submit"
+                            >
+                                {intl.formatMessage({ id: "registration.register_button" })}
+                            </Button>
+
+                            {message && (
+                                <Text mt={2} textAlign="center" color="orange.600" fontWeight="medium">
+                                    {message}
+                                </Text>
+                            )}
+                        </VStack>
                     </form>
                 </Box>
             </Flex>
