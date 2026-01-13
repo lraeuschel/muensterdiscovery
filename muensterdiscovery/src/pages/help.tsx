eb1864efab55b554d3d40f6a784a221277882b65
lraeusch <l.raeuschel@uni-muenster.de>
2026-01-13 18:52:50 +0100

    Addes some working stuff

--- muensterdiscovery/src/pages/help.tsx
+++ muensterdiscovery/src/pages/help.tsx
@@ -1,6 +1,36 @@
 import Header from "../components/Header";
+import React from "react";
+import { useEffect } from "react";
+import { addUserAchievement } from "../services/DatabaseConnection";
+import { getCurrentUser } from "../services/DatabaseConnection";
+import { getRoutes } from "../services/DatabaseConnection";
 
 export default function Help() {
+    const [userId, setUserId] = React.useState<string>("");
+    const [routes, setRoutes] = React.useState<any[]>([]);
+
+    useEffect(() => {
+        const loadUser = async () => {
+            const user = await getCurrentUser();
+            if (user) {
+                setUserId(user.id);
+            }
+        };
+        loadUser();
+    }, []);
+
+    addUserAchievement(userId, 1);
+
+    useEffect(() => {
+        const fetchRoutes = async () => {
+            const fetchedRoutes = await getRoutes();
+            setRoutes(fetchedRoutes);
+        };
+        fetchRoutes();
+    }, []);
+
+
+    console.log("Current user in Help page:", userId);
     return (
         <div>
             <Header />
